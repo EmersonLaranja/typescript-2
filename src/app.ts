2ba2daa93ea84662fe8cec8a76932f9bcd5c2518
EmersonLaranja <emersonlaranja@gmail.com>
2023-11-13 19:39:58 -0300

    4.1

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,21 @@
 import express from "express";
-import router from "./routes";
 import "reflect-metadata";
+import "express-async-errors";
 import { AppDataSource } from "./config/dataSource";
+import router from "./routes";
+import { erroMiddleware } from "./middleware/erro";
+
 const app = express();
 app.use(express.json());
+
 router(app);
+
+app.get("/teste", () => {
+  throw new Error("Erro teste");
+});
+
+app.use(erroMiddleware);
+
 
 AppDataSource.initialize()
   .then(() => {
